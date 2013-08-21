dc5d5bceae7b90afe5c0b226b07472d56484b7eb
Niko Välimäki <nvalimak@asdf.(none)>
2013-08-21 11:36:30 +0300

    Added missing files

---
+++ dependencies.mk
@@ -0,0 +1,34 @@
+BitRank.o: BitRank.cpp BitRank.h Tools.h
+CGkArray.o: CGkArray.cpp CGkArray.h BlockArray.h Tools.h ArrayDoc.h \
+ HuffWT.h BitRank.h incbwt/bits/deltavector.h incbwt/bits/bitvector.h \
+ incbwt/bits/../misc/definitions.h incbwt/bits/bitbuffer.h \
+ libcds/includes/basics.h libcds/includes/static_bitsequence.h \
+ libcds/includes/static_bitsequence_rrr02.h \
+ libcds/includes/table_offset.h \
+ libcds/includes/static_bitsequence_rrr02_light.h \
+ libcds/includes/static_bitsequence_naive.h \
+ libcds/includes/static_bitsequence_brw32.h \
+ libcds/includes/static_bitsequence_sdarray.h libcds/includes/sdarray.h
+HuffWT.o: HuffWT.cpp HuffWT.h BitRank.h Tools.h
+Tools.o: Tools.cpp Tools.h
+builder.o: builder.cpp bcr-demo.h CGkArray.h BlockArray.h Tools.h \
+ ArrayDoc.h HuffWT.h BitRank.h incbwt/bits/deltavector.h \
+ incbwt/bits/bitvector.h incbwt/bits/../misc/definitions.h \
+ incbwt/bits/bitbuffer.h libcds/includes/basics.h \
+ libcds/includes/static_bitsequence.h \
+ libcds/includes/static_bitsequence_rrr02.h \
+ libcds/includes/table_offset.h \
+ libcds/includes/static_bitsequence_rrr02_light.h \
+ libcds/includes/static_bitsequence_naive.h \
+ libcds/includes/static_bitsequence_brw32.h \
+ libcds/includes/static_bitsequence_sdarray.h libcds/includes/sdarray.h
+cgkquery.o: cgkquery.cpp CGkArray.h BlockArray.h Tools.h ArrayDoc.h \
+ HuffWT.h BitRank.h incbwt/bits/deltavector.h incbwt/bits/bitvector.h \
+ incbwt/bits/../misc/definitions.h incbwt/bits/bitbuffer.h \
+ libcds/includes/basics.h libcds/includes/static_bitsequence.h \
+ libcds/includes/static_bitsequence_rrr02.h \
+ libcds/includes/table_offset.h \
+ libcds/includes/static_bitsequence_rrr02_light.h \
+ libcds/includes/static_bitsequence_naive.h \
+ libcds/includes/static_bitsequence_brw32.h \
+ libcds/includes/static_bitsequence_sdarray.h libcds/includes/sdarray.h
